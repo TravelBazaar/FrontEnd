ae4669f62557750c31659d49973e4f3a90b0c1d8
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2024-06-25 11:33:29 -0500

    Update environment.ts

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  apiUrl: 'https://backendtravelbazaar-3krb.onrender.com/api/travelbazaar/v1',
+  apiUrl:'https://travelbazar.zeabur.app/api/travelbazaar/v1',
+ // apiUrl: 'https://backendtravelbazaar-3krb.onrender.com/api/travelbazaar/v1',
   localURL: 'http://localhost:8080/api/v1',
 };
+//https://travelbazar.zeabur.app/api/travelbazaar/v1/users
